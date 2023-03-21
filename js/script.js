f2630fe57aab31427172e4f73fe2bd54a7f775c6
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-03-21 16:17:31 +0000

    webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Yuri G
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+function doMathClicked() {
+  document.getElementById('multiply-math').innerHTML = "<p>The area is: " + (5*3) + "</p>";
+  document.getElementById('add-multiply-math').innerHTML = "<p>The perimeter is: " + (2+5*3) + "</p>";
+}
